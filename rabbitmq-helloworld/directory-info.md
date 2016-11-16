70d7ca4d4c3d6292bad63ea19f4fb1f281496a48
audeSt <audestick@gmail.com>
2016-11-16 15:51:34 +0800

    demo

--- rabbitmq-helloworld/directory-info.md
+++
@@ -1,3 +0,0 @@
-####这个文件夹是关于rabbitmq-cient的基础使用
-
-####参考的是https://audest.gitbooks.io/rabbitmq_tutorials_with_java/content/hello_world.html
--- src/main/java/mqhelloworld/Recv.java
+++ src/main/java/mqhelloworld/Recv.java
@@ -1,6 +1,9 @@
 package mqhelloworld;
 
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by lrkin on 2016/11/16.
@@ -8,7 +11,23 @@ import com.rabbitmq.client.ConnectionFactory;
 public class Recv {
     private final static String QUEUE_NAME = "hello";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [x] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };//consumer封装了(回调)消费过程
+
+        channel.basicConsume(QUEUE_NAME, true, consumer);//隧道消费这个消息
     }
 }
