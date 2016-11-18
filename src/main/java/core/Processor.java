4734221e03df5983acfbeb6ff2caae3496e75e88
audeSt <audestick@gmail.com>
2016-11-18 21:49:10 +0800

    Saver-interface

--- src/main/java/core/Processor.java
+++ src/main/java/core/Processor.java
@@ -1,10 +1,23 @@
 package core;
 
+import mq.MessageQueue;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Created by lrkin on 2016/11/16.
- *
- *
+ * <p>
+ * 解析器的接口.解析器用于提取关键信息,使用jsoup或者正则表达式完成任务
  */
 public interface Processor {
 
+    /**
+     * 处理下载下来的页面源码
+     *
+     * @param page HTML源代码
+     * @param map  提供把消息发送到各个消息队列的方法
+     * @throws IOException
+     */
+    public void process(Object page, Map<String, MessageQueue> map) throws IOException;
 }
--- src/main/java/core/Saver.java
+++ src/main/java/core/Saver.java
@@ -1,7 +1,22 @@
 package core;
 
+import mq.MessageQueue;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Created by lrkin on 2016/11/16.
+ *
+ * 数据持久化的接口
  */
 public interface Saver {
+
+    /**
+     * 处理最终解析得到的结果
+     * @param result 消息队列推送过来的结果消息
+     * @param map
+     * @throws IOException
+     */
+    public void save(Object result, Map<String, MessageQueue> map) throws IOException;
 }
